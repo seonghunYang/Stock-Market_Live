a8babe0ffcd8aa3ba2431854da3cb2a34cebd72c
seonghunYang <didtjdgns852@naver.com>
2020-04-24 01:25:47 +0900

    feat ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {request} from 'request';
+import Layout from './components/Layout';
+// const s = require('request');
+
+
+// s('https://finnhub.io/api/v1/stock/symbol?exchange=US&token=bqgqrufrh5r8lcmqasig', { json: true }, (err, res, body) => {
+//   if (err) { return console.log(err); }
+//   console.log(body.url);
+//   console.log(body.explanation);
+// });
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Layout />
+      </div>
   );
 }
 
