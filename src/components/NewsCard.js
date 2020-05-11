581691c488290644eeb3c74bf138374c8f06c304
seonghunYang <didtjdgns852@naver.com>
2020-05-12 07:11:30 +0900

    newpage 제작중

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -12,38 +12,46 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import CardHeader from '@material-ui/core/CardHeader';
 
-const useStyles = makeStyles({
+
+const useStyles = makeStyles((theme) => ({
   root: {
+    width: "100%",
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+    [theme.breakpoints.up('md')]: {
+      width: "60%"
+    }    
   },
-});
+  })
+)
 
 export default function NewsCard({ news }) {
   const classes = useStyles();
   let date = new Date(news.datetime);
   date = date.toString().slice(0,25);
   return (
-    <Card className={classes.root}>
-      <CardActionArea href={news.url}>
-        <CardHeader
-          title={news.headline}
-          subheader={date}
-        />
-        <CardMedia
-          component="img"
-          alt="news image"
-          height="140"
-          image={news.image}
-          title="news image"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="subtitle1" component="h2">
-            category: {news.category}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {news.summary}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+      <Card className={classes.root}>
+        <CardActionArea href={news.url}>
+          <CardHeader
+            title={news.headline}
+            subheader={date}
+          />
+          <CardMedia
+            component="img"
+            alt="news image"
+            height="300"
+            image={news.image}
+            title="news image"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="subtitle1" component="h2">
+              category: {news.category}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {news.summary}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
   );
 }
--- src/pages/NewsPage.js
+++ src/pages/NewsPage.js
@@ -4,10 +4,24 @@ import Container from '@material-ui/core/Container';
 import {CreateGeneralNews} from '../actions/index';
 import { useDispatch, useSelector } from 'react-redux'
 import NewsCard from '../components/NewsCard';
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default function NewsPage() {
   const dispatch = useDispatch();
   const generalNews = useSelector(state => state.generalNews);
+  const [hasMore, setHasMore] = React.useState(true);
+  const [items, setItems] = React.useState(Array.from({length:3}));
+
+  const isMoreData = () => {
+    if (items.length >= 20) {
+      setHasMore(false);
+      return;
+    }
+    setTimeout(() => {
+      setItems(items.concat(Array.from( {length:5 })));
+    }, 500);
+  }
+
   useEffect(() => {
     if(!generalNews){
       dispatch(CreateGeneralNews());
@@ -16,8 +30,21 @@ export default function NewsPage() {
   return (
     <>
       {generalNews &&
-        <Container maxWidth="md">
-          {generalNews.map(news => <NewsCard key={news.id} news={news} />)}
+        <Container maxWidth="md" style={{display:"flex", flexDirection:"column", alignItems:'center'}}>
+          <InfiniteScroll
+            dataLength={5}
+            next={isMoreData}
+            hasMore={hasMore}
+            height={300}
+            loader={<div style={{ textAlign: "center" }}>Loading..</div>}
+            endMessage={
+              <p style={{ textAlign: "center" }}>
+                <b>Yay! You have seen it all</b>
+              </p>
+            }
+          >        
+            {generalNews.map(news => <NewsCard key={news.id} news={news} />)}
+          </InfiniteScroll>
         </Container>
       }
     </>
